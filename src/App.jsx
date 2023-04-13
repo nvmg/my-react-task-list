596ec4460ff0c2919c31523f8674ad888be2c7b5
nvmg <nicole.montana@mail.escuelaing.edu.co>
2023-04-13 03:25:58 -0500

    Cuarta parte

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,23 @@ function App() {
   const [initialTasks, addTask, deleteTask, deleteAllTasks, changeStateTask] = useManageTasks();
   const [value, setValue] = useState("");
   const [pending, calculatePending, sumOne, minusOne, reset] = useTaskList();
+  const [formValidation, setFormValidation] = useState({task:undefined})
+
+  function handleChange(event) {
+    const val = event.target.value;
+    setFormValidation({
+      ...formValidation,
+      task:val.length < 3? "task is too short":"",
+    })
+    setValue(val);
+
+  }
 
   const handleAdd = () => {
     addTask(value);
     sumOne();
     setValue("");
+    handleCloseModal();
   }
 
   const handleDeleteAll = () => {
@@ -31,14 +43,27 @@ function App() {
   }
 
   const newValue = (event) =>{
-    setValue(event.target.value);
+    const val = event.target.value;
+    setFormValidation({
+      ...formValidation,
+      task:val.length < 3? "task is too short":"",
+    })
+    setValue(val);
   }
+
+  const handleCloseModal = () => {
+    setFormValidation({task:undefined,description:undefined})
+    setIsOpen(false);
+  };
+
+  const isValid = Object.keys(formValidation).every(key=>formValidation[key]==="")
 
   return (
     <div className="App">
       <Header />
       <form onSubmit={handleAdd}>
         <input placeholder="Add your new todo" type="text" id = "newTask" onChange={newValue} value={value}></input>
+        <span role = "alert" style={{color:"red"}}>{formValidation.task}</span>
         <button style={{color: 'blue'}} type="submit">+</button>
       </form>
       <TaskList list={initialTasks} onDeleteOne={handleDeleteOne} onChangeState={handleChangeState}/>
